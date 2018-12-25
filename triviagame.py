8e0ae7a05bd6e1ce78065c170388af8efc250904
lara <larakaracasu@gmail.com>
2018-12-25 22:05:01 +0000

    Removed dictionaries; replaced with Question class.

--- triviagame.py
+++ triviagame.py
@@ -1,4 +1,6 @@
 import random
+import time
+import site
 rung_number = 1
 name = input("Welcome to Trivia Ladder! What's your name?")
 print(f"Hello, {name}! The object of the game is to use your *worldly knowledge* to climb your way to the top! There are four basic categories: foreign languages, science, and geography. At each rung of the ladder, a category will be selected at random, and a multiple-choice question will be asked. Answer correctly, and you will move up one rung. Answer incorrectly, and you will move down on the ladder. Questions will become progressively more difficult. Make it all the way to Rung 15, and you win! Fall off the ladder, and you lose!")
@@ -37,26 +39,31 @@ easy_questions = {
   }
 }
 
-easy_questions_list = ["What is intracellular?", "Whomst?", "Where is zika virus", "What is cell?"]
+easy_questions_list = ["What is intracellular?", "Whomst?", "Where is zika virus", "What is cell?", "What even?", "Ok"]
 
 medium_questions_list = ["What is the capital of Ohio?", "What is opposite the color red on the color wheel?", "tdtfdyt", "yhfy", "dty", "hiu", "ygyhguk", "re5rswerwer", "hdtdty"]
 
 hard_questions_list = ["What is?", "Whomst?", "serser", "ygyugyug", "fyfuygui", "uhoiu", "ujhou", "esre", "tug", "ygg8o8u"]
 
 while rung_number >= 1 and rung_number != 15:
-  if rung_number >= 1 and rung_number <= 6:
-    question = random.choice(easy_question_list)
+  if rung_number >= 1 and rung_number < 6:
+    question = random.choice(easy_questions_list)
     easy_questions_list.remove(question)
-
-  if rung_number > 6 and rung_number <= 10:
+  if rung_number >= 6 and rung_number <= 10:
     question = random.choice(medium_questions_list)
     medium_questions_list.remove(question)
   if 10 < rung_number and rung_number <= 15:
     question = random.choice(hard_questions_list)
     hard_questions_list.remove(question)
-
+    question = random.choice(hard_questions_list)
+    hard_questions_list.remove(question)
   correct_answer = "yes"
+  starttime = time.time()
   player_answer = input(question)
+  endtime = time.time()
+  if (endtime - starttime) > 15:
+    print(f"You took too long ({(endtime - starttime)} seconds)! You lost the game...")
+    break
   if player_answer == correct_answer:
     rung_number += 1
     print(f"CORRECT. You are now on Rung {rung_number}.")
@@ -65,7 +72,7 @@ while rung_number >= 1 and rung_number != 15:
     print(f"INCORRECT. You are now on Rung {rung_number}.")
   elif (rung_number - fall_number) <= 0:
     rung_number -= fall_number
-    print("You lost the game :( ")
+    print("You lost the game...")
 
 if rung_number == 15:
   print("CONGRATULATIONS! You won the game!")
