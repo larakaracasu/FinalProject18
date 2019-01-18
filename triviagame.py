55a23212043730d4dab822df123a247f01f84bca
lara <larakaracasu@gmail.com>
2019-01-18 01:04:25 +0000

    Class structure with question.answer is not working -- Final commit before Classes and lists are rearranged

--- triviagame.py
+++ triviagame.py
@@ -30,21 +30,21 @@ class Question:
         self.answer = answer
 
 easy_world_questions = [
-    "How do you say 'good morning' in Italian?\n(a) Buona notte\n(b) buenas noches\n(c) buongiorno\n(d)boa noite",
-    "How do you say 'hunger' in Spanish?\n(a) hombre\n(b) hambre\n(c) hungre\n(d) humbre",
+    "How do you say 'good morning' in Italian?\n(a) Buona notte\n(b) Buenas noches\n(c) Buongiorno\n(d) Boa noite",
+    "How do you say 'hunger' in Spanish?\n(a) Hombre\n(b) Hambre\n(c) Hungre\n(d) Humbre",
     "What is 'welcome' in French?\n(a) Bienvenue\n(b) Bienvenido\n(c) Bienvienue\n(d) Bienvena",
     "Which of these countries DOES NOT border Italy?\n(a) France\n(b) Austria\n(c) Slovenia\n(d) Germany",
-    "Which of these countries is ?\n(a) thing1\n(b) thing2",
-    "How do you say '' in ?\n(a) \n(b) \n(c) \n(d) ",
+    "Which of these places is NOT an actual country?\n(a) Bhutan\n(b) Maltas\n(c) Burundi\n(d) Nauru",
+    "Which of these places IS a country as recognized by \n(a) \n(b) \n(c) \n(d) ",
 ]
 
 easy_world_answers = [
     Question(easy_world_questions[0], "a"),
     Question(easy_world_questions[1], "b"),
     Question(easy_world_questions[2], "a"),
-    Question(easy_world_questions[3], "a"),
-    Question(easy_world_questions[4], "a"),
-    Question(easy_world_questions[5], "b"),
+    Question(easy_world_questions[3], "d"),
+    Question(easy_world_questions[4], "c"),
+    Question(easy_world_questions[5], "c"),
 ]
 
 easy_science_questions = [
@@ -72,20 +72,19 @@ medium_world_questions = [
     "Which of these countries borders Thailand?\n(a) China\n(b) Vietnam \n(c) Cambodia \n(d) Singapore",
     "Which of these countries is NOT in Central America?\n(a) Belize\n(b) Costa Rica\n(c) Cuba\n(d) Guatemala",
     "Which of these countries is NOT located in Asia?\n(a) Sri Lanka\n(b) Bahrain\n(c) Algeria\n(d) Myanmar",
-    "hdtdmedty\n(a) thing1\n(b) thing2,
 ]
 
 medium_world_answers = [
     Question(medium_world_questions[0], "a"),
-    Question(medium_world_questions[1], "b"),
+    Question(medium_world_questions[1], "d"),
     Question(medium_world_questions[2], "a"),
-    Question(medium_world_questions[3], "a"),
-    Question(medium_world_questions[4], "b"),
-    Question(medium_world_questions[5], "a"),
+    Question(medium_world_questions[3], "c"),
+    Question(medium_world_questions[4], "c"),
+    Question(medium_world_questions[5], "c"),
 ]
 
 medium_science_questions = [
-    "What is the capital of Venezuela?\n(a) Caracas\n(b) Asunción\n(c) Quito\n(d) Montevideo",
+    "Which of these options incorrectly pairs the ?\n(a) Caracas\n(b) Asunción\n(c) Quito\n(d) Montevideo",
     "Which of these cities is not in Turkey?\n(a) Gaziantep \n(b) Ankara \n(c) Istanbul \n(d) Patras",
     "How do you say “bye” in Portuguese?\n(a) tchau \n(b) adios \n(c) adieu \n(d) te vuelvas",
     "Which of these countries borders Thailand?\n(a) China\n(b) Vietnam \n(c) Cambodia \n(d) Singapore",
@@ -144,7 +143,8 @@ hard_science_answers = [
 
 easy_topics = [easy_world_questions, easy_science_questions]
 medium_topics = [medium_world_questions, medium_science_questions]
-hard_topics = [hard_world_questions, medium_science_questions]
+hard_topics = [hard_world_questions, hard_science_questions]
+
 
 while rung_number > 0 and rung_number != 15:
 
@@ -152,9 +152,11 @@ while rung_number > 0 and rung_number != 15:
         for question in easy_world_questions or easy_science_questions:
             easy_question_type = random.choice(easy_topics)
             if easy_question_type == easy_world_questions:
+                print("An EASY WORLD question has been SELECTED.")
                 prompt = random.choice(easy_world_questions)
                 easy_world_questions.remove(prompt)
             if easy_question_type == easy_science_questions:
+                print("An EASY SCIENCE question has been SELECTED.")
                 prompt = random.choice(easy_science_questions)
                 easy_science_questions.remove(prompt)
             if not easy_science_questions or not easy_world_questions:
@@ -185,13 +187,15 @@ while rung_number > 0 and rung_number != 15:
         for question in medium_world_questions or medium_science_questions:
             medium_question_type = random.choice(medium_topics)
             if medium_question_type == medium_world_questions:
+                print("A MEDIUM WORLD question has been SELECTED.")
                 prompt = random.choice(medium_world_questions)
                 medium_world_questions.remove(prompt)
             if medium_question_type == medium_science_questions:
+                print("A MEDIUM SCIENCE question has been SELECTED.")
                 prompt = random.choice(medium_science_questions)
                 medium_science_questions.remove(prompt)
             if not medium_science_questions or not medium_world_questions:
-                print("You've fallen too many times...it seems as though the ladder is breaking!\nYou have lost.")
+                print("You've fallen too many times...it seems as though the ladder is breaking!\nYou have lost...")
 
             starttime = time.time()
             player_answer = input(prompt)
@@ -218,9 +222,11 @@ while rung_number > 0 and rung_number != 15:
         for question in hard_world_questions or hard_science_questions:
             hard_question_type = random.choice(hard_topics)
             if hard_question_type == hard_world_questions:
+                print("A HARD WORLD question has been SELECTED.")
                 prompt = random.choice(hard_world_questions)
                 hard_world_questions.remove(prompt)
             if hard_question_type == hard_science_questions:
+                print("A HARD SCIENCE question has been SELECTED.")
                 prompt = random.choice(hard_science_questions)
                 hard_science_questions.remove(prompt)
             if not hard_science_questions or not hard_world_questions:
